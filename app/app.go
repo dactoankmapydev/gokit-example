2d47fa5a6484b722889d5d273544b52621ca9af7
Nguyễn Đắc Toàn <30865156+dactoankmapydev@users.noreply.github.com>
2019-08-08 10:41:47 +0700

    Update app.go

--- app/app.go
+++ app/app.go
@@ -1,3 +1,13 @@
 package app
 
-type App struct{}
+type App struct {
+	Id            string `json:"id"`
+	Platform      string `json:"platform"`
+	BundleId      string `json:"bundleId"`
+	PackageName   string `json:"packageName"`
+	Name          string `json:"name"`
+	GooglePlayUrl string `json:"googlePlayUrl"`
+	AppStoreUrl   string `json:"appStoreUrl"`
+	Icon          string `json:"icon"`
+	Version       string `json:"version"`
+}
