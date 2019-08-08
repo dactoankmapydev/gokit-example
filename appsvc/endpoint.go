1acfa41538e436fcf41f53038f33c73158d13e4b
Nguyễn Đắc Toàn <30865156+dactoankmapydev@users.noreply.github.com>
2019-08-08 10:41:02 +0700

    Update endpoint.go

--- appsvc/endpoint.go
+++ appsvc/endpoint.go
@@ -11,3 +11,15 @@ func makeGetAppsEndpoint(s Service) endpoint.Endpoint {
 		return s.GetApps(ctx, req.(GetAppsRequest))
 	}
 }
+
+func makeRegisterAppEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, req interface{}) (interface{}, error) {
+		return s.RegisterApp(ctx, req.(RegisterAppRequest))
+	}
+}
+
+// func makeGetAppDetailEndpoint(s Service) endpoint.Endpoint {
+// 	return func(ctx context.Context, req interface{}) (interface{}, error) {
+// 		return s.GetAppDetail(ctx, req.(GetAppDetailRequest))
+// 	}
+// }
