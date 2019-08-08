81deeec10b943c636e0b9670b955cad845393b31
Nguyễn Đắc Toàn <30865156+dactoankmapydev@users.noreply.github.com>
2019-08-08 10:40:39 +0700

    Update request_response.go

--- appsvc/request_response.go
+++ appsvc/request_response.go
@@ -4,10 +4,44 @@ import (
 	"miniapp_backend/app"
 )
 
+type Cursor struct {
+	Next string `json:"next"`
+	Prev string `json:"prev"`
+}
+
 type GetAppsRequest struct {
-	Limit int
+	Limit  int    `json:"limit"`
+	Cursor string `json:"cursor"`
 }
+
 type GetAppsResponse struct {
-	Total int
-	Apps  []app.App
+	Total  int       `json:"total"`
+	Apps   []app.App `json:"apps"`
+	Cursor Cursor    `json:"cursor"`
 }
+
+type RegisterAppRequest struct {
+	Id            string `json:"id"`
+	Platform      string `json:"platform"`
+	BundleId      string `json:"bundleId"`
+	PackageName   string `json:"packageName"`
+	Name          string `json:"name"`
+	GooglePlayUrl string `json:"googlePlayUrl"`
+	AppStoreUrl   string `json:"appStoreUrl"`
+	Icon          string `json:"icon"`
+	Version       string `json:"version"`
+}
+
+type RegisterAppResponse struct {
+	App app.App `json:"app"`
+	Ica string  `json:"ica"`
+}
+
+// type GetAppDetailRequest struct {
+// 	Id string `json:"string"`
+// }
+
+// type GetAppDetailResponse struct {
+// 	App app.App `json:"app"`
+// 	ica string  `json:"string"`
+// }
