ee4a9f78687e1e5c0024448ce97afd33c9433786
Nguyễn Đắc Toàn <30865156+dactoankmapydev@users.noreply.github.com>
2019-08-16 01:25:08 +0700

    Update request_response.go

--- appsvc/request_response.go
+++ appsvc/request_response.go
@@ -9,39 +9,98 @@ type Cursor struct {
 	Prev string `json:"prev"`
 }
 
-type GetAppsRequest struct {
-	Limit  int    `json:"limit"`
-	Cursor string `json:"cursor"`
+type GetMainAppRequest struct {
+	Limit     int
+	Offset    int
+	Cursor    string
+	Ownership string
 }
 
-type GetAppsResponse struct {
-	Total  int       `json:"total"`
-	Apps   []app.App `json:"apps"`
-	Cursor Cursor    `json:"cursor"`
+type GetMainAppResponse struct {
+	Total  int           `json:"total"`
+	Apps   []app.MainApp `json:"apps"`
+	Cursor Cursor        `json:"cursor"`
 }
 
 type RegisterAppRequest struct {
-	Id            string `json:"id"`
-	Platform      string `json:"platform"`
-	BundleId      string `json:"bundleId"`
-	PackageName   string `json:"packageName"`
-	Name          string `json:"name"`
-	GooglePlayUrl string `json:"googlePlayUrl"`
-	AppStoreUrl   string `json:"appStoreUrl"`
-	Icon          string `json:"icon"`
-	Version       string `json:"version"`
+	Platform      app.Platform
+	BundleId      string
+	PackageName   string
+	Name          string
+	GooglePlayUrl string
+	AppStoreUrl   string
+	Icon          string
+	Version       string
 }
 
 type RegisterAppResponse struct {
-	App app.App `json:"app"`
-	Ica string  `json:"ica"`
+	App app.MainApp `json:"app"`
+	Ica string      `json:"ica"` // intermediate cert
 }
 
-// type GetAppDetailRequest struct {
-// 	Id string `json:"string"`
-// }
+type GetAppDetailRequest struct {
+	Id string
+}
 
-// type GetAppDetailResponse struct {
-// 	App app.App `json:"app"`
-// 	ica string  `json:"string"`
-// }
+type GetAppDetailResponse struct {
+	App app.MainApp `json:"app"`
+	Ica string      `json:"ica"`
+}
+
+type GetMiniAppOfAppRequest struct {
+	Limit  int
+	Cursor string
+	Status app.Status
+}
+
+type GetMiniAppOfAppResponse struct {
+	Total  int           `json:"total"`
+	Apps   []app.MiniApp `json:"apps"`
+	Cursor Cursor        `json:"cursor"`
+}
+
+type UpdateMiniAppOfAppRequest struct {
+	MainAppID string
+	MiniAppID string
+	Status    app.MiniAppType
+}
+
+type GetMiniAppRequest struct {
+	Limit  int
+	Offset int
+	Cursor string
+	Status app.MiniAppType
+}
+
+type GetMiniAppResponse struct {
+	Total  int           `json:"total"`
+	Apps   []app.MiniApp `json:"apps"`
+	Cursor Cursor        `json:"cursor"`
+}
+
+type GetMiniAppDetailRequest struct {
+	Id string
+}
+
+type GetMiniAppDetailResponse struct {
+	App     app.MiniApp `json:"app"`
+	History []string    `json:"history"`
+}
+
+type CreateMiniAppRequest struct {
+	Platform      app.Platform
+	BundleId      string
+	PackageName   string
+	DisplayName   string
+	AppName       string
+	Type          app.MiniAppType
+	TargetVersion string
+	Icon          string
+	Version       string
+	Permissions   []string
+	Bundle        app.BundleType
+}
+
+type CreateMiniAppResponse struct {
+	App app.MiniApp `json:"app"`
+}
