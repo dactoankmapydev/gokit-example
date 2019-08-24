9d1655e3e8c1ebd168454d5cd7ea783c289b734e
Nguyễn Đắc Toàn <30865156+dactoankmapydev@users.noreply.github.com>
2019-08-24 11:50:10 +0700

    Update app.go

--- app/app.go
+++ app/app.go
@@ -1,51 +1,9 @@
 package app
 
-// Platform
-type Platform string
-
-const (
-	Android Platform = "android"
-	Ios     Platform = "ios"
-)
-
-// Status
-type Status string
-
-const (
-	Develop   Status = "develop"
-	Staging   Status = "staging"
-	Published Status = "published"
-)
-
-// MiniAppType
-type MiniAppType string
-
-const (
-	InApp      MiniAppType = "in-app"
-	Background MiniAppType = "background"
-)
-
-// MiniAppStatus
-type MiniAppStatus string
-
-const (
-	Approved MiniAppStatus = "approved"
-	Rejected MiniAppStatus = "rejected"
-	Pending  MiniAppStatus = "pending"
-)
-
-// BundleType
-type BundleType string
-
-const (
-	Multipart BundleType = "multipart"
-	Formdata  BundleType = "form-data"
-)
-
 // App
 type MainApp struct {
 	Id            string   `json:"id"`
-	Platform      Platform `json:"platform"`
+	Platform      string   `json:"platform"`
 	BundleId      string   `json:"bundleId"`
 	PackageName   string   `json:"packageName"`
 	Name          string   `json:"name"`
@@ -53,22 +11,21 @@ type MainApp struct {
 	AppStoreUrl   string   `json:"appStoreUrl"`
 	Icon          string   `json:"icon"`
 	Version       string   `json:"version"`
-	Events        []string `json:"event"`
+	Events        []string `json:"events"`
 }
 
 // MiniApp
 type MiniApp struct {
-	Id            string      `json:"id"`
-	Platform      Platform    `json:"platform"`
-	BundleId      string      `json:"bundleId"`
-	PackageName   string      `json:"packageName"`
-	DisplayName   string      `json:"displayname"`
-	AppName       string      `json:"appName"`
-	Status        Status      `json:"status"`
-	Type          MiniAppType `json:"type"`
-	Icon          string      `json:"icon"`
-	TargetVersion string      `json:"targetVersion"`
-	Version       string      `json:'version"`
-	Bundle        BundleType  `json:'bundle"`
-	Permissions   []string    `json:"permissions"`
+	Id            string   `json:"id"`
+	Platform      string   `json:"platform"`
+	BundleId      string   `json:"bundleId"`
+	PackageName   string   `json:"packageName"`
+	DisplayName   string   `json:"displayname"`
+	AppName       string   `json:"appName"`
+	Status        string   `json:"status"`
+	Type          string   `json:"type"`
+	Icon          string   `json:"icon"`
+	TargetVersion string   `json:"targetVersion"`
+	Version       string   `json:"version"`
+	Permissions   []string `json:"permissions"`
 }
