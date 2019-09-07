25597eb13f0edb90051b312673ad292e7c5dcf9e
Nguyễn Đắc Toàn <30865156+dactoankmapydev@users.noreply.github.com>
2019-09-07 16:07:41 +0700

    Update app.go

--- app/app.go
+++ app/app.go
@@ -1,24 +1,21 @@
 package app
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
-// import (
-// 	"go.mongodb.org/mongo-driver/bson/primitive"
-// )
-
-// primitive.ObjectID
 // App
 type MainApp struct {
-	Id            primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Platform      string             `json:"platform" bson:"platform"`
-	BundleId      string             `json:"bundleId" bson:"bundleId"`
-	PackageName   string             `json:"packageName" bson:"packageName"`
-	Name          string             `json:"name" bson:"name"`
-	GooglePlayUrl string             `json:"googlePlayUrl" bson:"googlePlayUrl"`
-	AppStoreUrl   string             `json:"appStoreUrl" bson:"appStoreUrl"`
-	Icon          string             `json:"icon" bson:"icon"`
-	Version       string             `json:"version" bson:"version"`
-	Events        []string           `json:"events" bson:"events"`
+	Id            string   `json:"id,omitempty" bson:"id,omitempty"`
+	Platform      string   `json:"platform" bson:"platform"`
+	BundleId      string   `json:"bundleId" bson:"bundleId"`
+	PackageName   string   `json:"packageName" bson:"packageName"`
+	Name          string   `json:"name" bson:"name"`
+	GooglePlayUrl string   `json:"googlePlayUrl" bson:"googlePlayUrl"`
+	AppStoreUrl   string   `json:"appStoreUrl" bson:"appStoreUrl"`
+	Icon          string   `json:"icon" bson:"icon"`
+	Version       string   `json:"version" bson:"version"`
+	Events        []string `json:"events" bson:"events"`
 }
 
 // MiniApp
